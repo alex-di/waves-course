9405f23483414c23bdaf52c9ac22995b4cc5da0a
Aleksei Diakov <mm77707@gmail.com>
2019-05-23 11:32:24 +0300

    Added bootstrap

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 function auth() {
   WavesKeeper
     ? WavesKeeper.auth({ data: "some dta"})
@@ -7,7 +9,13 @@ function auth() {
 }
 
 export default function () {
-  return (<div>
-    <input type="submit" onClick={auth}/>
+  return (<div className="container">
+    <Head>
+      <meta charset="UTF-8" />
+      <title>Mastering Web3 with Waves boilerplte</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+    </Head>
+    <h1>dAPP fetus</h1>
+    <input className="btn btn-primary" type="submit" onClick={auth}/>
   </div>)
 }
